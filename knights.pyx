f37c9c473590367c8a38974c6d720aecbab498cb
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2011-12-05 14:02:01 +0100

    faster algorithm for knights

--- knights.pyx
+++ knights.pyx
@@ -1,16 +1,19 @@
-from nim cimport nimproduct, nimpower
+from nim cimport nimproduct
 
-cpdef unsigned knights(unsigned size, unsigned start, unsigned stop, unsigned previous):
-    cdef unsigned n, t, test
-    cdef bint bad
-    for n in range(start, stop):
-        bad = False
-        for t in range(1, size):
-            test = nimproduct(n, n ^ t)
-            if test < size and test != previous:
-                bad = True
-                break
-        if bad:
-            continue
-        if nimpower(n, size + 1) == previous:
-            return n
+cpdef unsigned knights(unsigned size, unsigned previous,
+                       unsigned start=1, unsigned stop=0):
+    cdef unsigned t, m, n0
+    if stop == 0:
+        stop = size + 1
+    checked = set()
+    for t in range(start, stop):
+        m = nimproduct(size / 2, nimproduct(t, t))
+        checked.clear()
+        for n0 in range(size):
+            if n0 in checked:
+                checked.remove(n0)
+                continue
+            checked.add(n0 ^ t)
+            if m ^ nimproduct(n0, n0 ^ t) == previous:
+                return t * size + n0
+
