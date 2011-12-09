29dcd6b0a53aa6433ad03a2e01cfbaa5b5f27bb8
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2011-12-09 11:24:28 +0100

    faster knights

--- knights.pyx
+++ knights.pyx
@@ -1,19 +1,48 @@
+#cython: cdivision=True
 from nim cimport nimproduct
+from libc.stdlib cimport malloc, free
 
-cpdef unsigned knights(unsigned size, unsigned previous,
-                       unsigned start=1, unsigned stop=0):
-    cdef unsigned t, m, n0
-    if stop == 0:
-        stop = size + 1
-    checked = set()
-    for t in range(start, stop):
-        m = nimproduct(size / 2, nimproduct(t, t))
-        checked.clear()
-        for n0 in range(size):
-            if n0 in checked:
-                checked.remove(n0)
-                continue
-            checked.add(n0 ^ t)
-            if m ^ nimproduct(n0, n0 ^ t) == previous:
-                return t * size + n0
+cpdef unsigned knights(unsigned size, unsigned previous):
+    cdef unsigned u, v, w, t, m, i, n0
+    for u in range(log(size)):
+        v = 2**u
+        for w in range(v):
+            t = v + w
+            m = nimproduct(size / 2, nimproduct(t, t))
+            for i in range(size / (2*v)):
+                for n0 in range(2*v*i, 2*v*i + v):
+                    if m ^ prod(n0, t) == previous:
+                        return t * size + n0
 
+prods = dict()
+
+cdef unsigned prod(unsigned n, unsigned t):
+    cdef unsigned res = 0
+    cdef unsigned short bc = bitcount(n), exp = 0, count = 0
+    cdef unsigned short* exps = <unsigned short*>malloc(bc * sizeof(unsigned short))
+    while n:
+        if n & 1:
+            exps[count] = exp
+            count += 1
+        exp += 1
+        n >>= 1
+    for exp in exps[:bc]:
+        if (t, exp) not in prods:
+            prods[t, exp] = nimproduct(2**exp, 2**exp ^ t)
+        res ^= prods[t, exp]
+    free(exps)
+    return res
+
+cdef unsigned short log(unsigned n):
+    cdef unsigned short l = 0
+    while n:
+        n <<= 1
+        l += 1
+    return l - 1
+
+cdef unsigned short bitcount(unsigned n):
+    cdef unsigned short count = 0
+    while n:
+        n &= n - 1
+        count += 1
+    return count
