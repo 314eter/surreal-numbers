5e6e960cc1e9796c0ccdd0b30218aef542eec62a
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2014-02-25 04:35:37 +0100

    Changes in knights.pyx

--- knights.pyx
+++ knights.pyx
@@ -1,28 +1,26 @@
+#cython: boundscheck=False
+#cython: wraparound=False
 #cython: cdivision=True
+#cython: overflowcheck=False
 from nim cimport nimproduct
 from libc.stdlib cimport malloc, free
 from libc.string cimport memset
 
 cdef unsigned long* prods
 
-cdef unsigned short log(unsigned long n):
-    cdef unsigned short l = 0
-    while n:
-        n <<= 1
-        l += 1
-    return l - 1
-
-cpdef unsigned long knights(unsigned long size, unsigned long previous):
-    cdef unsigned long v, w, t, m, i, n0
-    cdef unsigned short exp, u, l = log(size)
+cpdef unsigned long knight(unsigned short n, unsigned long previous, unsigned long start=0):
+    cdef unsigned long size, v, w, t, m, i, n0
+    cdef unsigned short exp, u, l = 2**n
     global prods
     prods = <unsigned long*>malloc(2 * l * sizeof(unsigned long))
-    for u in range(l):
+    size = 2**l
+    for u in range(l / 4, l):
         v = 2**u
         for w in range(v):
             t = v + w
+            print 't:', t
             memset(prods, 0, 2 * l * sizeof(unsigned long))
-            m = nimproduct(size / 2, nimproduct(t, t))
+            m = nimproduct(size >> 1, nimproduct(t, t))
             for i in range(size / (2*v)):
                 for n0 in range(2*v*i, 2*v*i + v):
                     if m ^ prod(n0, t) == previous:
@@ -30,7 +28,7 @@ cpdef unsigned long knights(unsigned long size, unsigned long previous):
                         return t * size + n0
 
 cdef unsigned long prod(unsigned long n, unsigned long t):
-    cdef unsigned long product, res = 0
+    cdef unsigned long res = 0
     cdef unsigned short exp = 0
     global prods
     while n:
